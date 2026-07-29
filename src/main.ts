332400af5400e97a6b19584ced63fc0d518ec0b3
Rolando Lazaro <rolandolazaro@gmail.com>
2026-07-29 11:44:43 -0500

    Para despliegue en la nube 03

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as dns from 'node:dns';
+
+// Forzar el uso de DNS públicos (Cloudflare / Google)
+dns.setDefaultResultOrder('ipv4first');
+dns.setServers(['1.1.1.1', '8.8.8.8']);
 
 async function main() {
   const app = await NestFactory.create(AppModule);
